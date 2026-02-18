13bac22c1c5c2c4d53960bbcb9542b90e75fdb00
unravelled-matt <154627148+unravelled-matt@users.noreply.github.com>
2026-02-18 10:30:30 +0000

    Try fix again

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,15 +68,6 @@ def test_get_nth_fibonacci_one():
     assert result == 1
 
 
-def test_get_nth_fibonacci_negative():
-   """Test with a negative number to raise ValueError."""
-   # Arrange
-   n = -1
-
-   # Act & Assert
-   with pytest.raises(ValueError):
-      get_nth_fibonacci(n)
-
 def test_get_nth_fibonacci_ten():
    """Test with n=10."""
    # Arrange
@@ -87,3 +78,13 @@ def test_get_nth_fibonacci_ten():
 
    # Assert
    assert result == 55
+
+
+def test_get_nth_fibonacci_negative():
+   """Test with a negative number to raise ValueError."""
+   # Arrange
+   n = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
